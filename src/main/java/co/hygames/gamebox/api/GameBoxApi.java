c028737c18482535dc75ec6c2d69ac04e6d69ac1
Niklas Eicker <nikle1@gmx.de>
2019-05-22 16:03:34 +0200

    begin database and api classes

---
+++ src/main/java/co/hygames/gamebox/api/GameBoxApi.java
@@ -0,0 +1,29 @@
+/*
+ * GameBox
+ * Copyright (C) 2019  Niklas Eicker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.hygames.gamebox.api;
+
+import co.hygames.gamebox.database.Callback;
+import co.hygames.gamebox.player.GbPlayer;
+
+import java.util.UUID;
+
+public interface GameBoxApi {
+    // ToDo: UUID for players. What is Hytale using?
+    void getPlayer(UUID playerId, Callback<GbPlayer> callback);
+}
